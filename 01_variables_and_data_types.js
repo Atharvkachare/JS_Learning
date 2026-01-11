95b5c811fc02c66ea876c2b2ddc022e499219928
Atharvkachare <atharvkachare9@gmail.com>
2026-01-11 21:16:14 +0530

    Learned how to declare variables

---
+++ 01_variables_and_data_types.js
@@ -0,0 +1,22 @@
+// Activity 1: Variable declaration
+
+// Task 1: Declare a variable named 'num' and assign it the value of 18. Then, print the value of 'num' to the console.
+var num= 18;
+console.log(num);
+
+console.log("***Task 1 Ended Here!***");
+
+// Task 2: Declare a variable using let , assign it a string , and log the value to the console.
+let myString  = "Atharv"
+console.log(myString);
+
+console.log("***Task 2 Ended Here!***");
+
+//Activity 2: Constsnt Declaration
+
+// Task 3: Declare a variable using const, assign it a boolean vaule, and log the value to the console.
+const Boolean = true;
+console.log(Boolean);
+
+console.log("***Task 3 Ended Here!***");
+
